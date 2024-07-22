00c0bfffd3c2346b3bb28c8be978f3ce07606199
joacodalmasso <joacodalmasso@gmail.com>
2024-07-22 23:37:47 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+const app = express();
+app.use(express.json());
+
+const port = parseInt(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
